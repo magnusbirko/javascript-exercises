802f8cd4c258438f02b04699ad8d971f3a57f934
magnusbirko <magnusbirko@gmail.com>
2024-11-07 18:49:54 +0100

    finished 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahrenheit) {
+  const convertToCelsius = Math.round((fahrenheit - 32) * (5 / 9) * 10) / 10;
+  return convertToCelsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celsius) {
+  const convertToFahrenheit = Math.round((celsius * (9 / 5) + 32) * 10) / 10;
+  return convertToFahrenheit;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };

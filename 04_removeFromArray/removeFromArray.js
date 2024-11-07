537c7a5d4ec1b34078e9a1175455e159e6339843
magnusbirko <magnusbirko@gmail.com>
2024-11-07 16:23:59 +0100

    finished 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...removeValues) {
+  let newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    if (!removeValues.includes(array[i])) {
+      newArray.push(array[i]);
+    }
+  }
+  return newArray;
 };
 
 // Do not edit below this line

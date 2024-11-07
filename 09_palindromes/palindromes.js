f9dc062cfe4247de4c5d7e1e81c5598cee81093f
magnusbirko <magnusbirko@gmail.com>
2024-11-07 21:17:58 +0100

    finished 10

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  const cleanStr = str.replace(/[^A-Za-z0-9]/g, "").toUpperCase();
+  return cleanStr === cleanStr.split("").reverse().join("");
 };
 
 // Do not edit below this line
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function (n) {
+  if (n < 0) {
+    return "OOPS";
+  } else if (n === 0) {
+    return 0;
+  } else if (n === 1) {
+    return 1;
+  } else {
+    let fib = [0, 1];
+    for (i = 2; i <= n; i++) {
+      fib[i] = fib[i - 2] + fib[i - 1];
+    }
+    return fib[n];
+  }
 };
-
 // Do not edit below this line
 module.exports = fibonacci;

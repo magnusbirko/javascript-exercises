07506d98b0a2781a10c4be838a0a623f5f75a787
magnusbirko <magnusbirko@gmail.com>
2024-11-07 19:13:56 +0100

    finished 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,45 @@
-const add = function() {
-	
+const add = function (...input) {
+  let sum = 0;
+  for (let i = 0; i < input.length; i++) {
+    sum += input[i];
+  }
+  return sum;
 };
 
-const subtract = function() {
-	
+const subtract = function (...input) {
+  let sum = input[0];
+  for (let i = 1; i < input.length; i++) {
+    sum -= input[i];
+  }
+  return sum;
 };
 
-const sum = function() {
-	
+const sum = function (input) {
+  let sum = 0;
+  for (let i = 0; i < input.length; i++) {
+    sum += input[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function (input) {
+  let sum = 1;
+  for (let i = 0; i < input.length; i++) {
+    sum *= input[i];
+  }
+  return sum;
 };
 
-const power = function() {
-	
+const power = function (number, power) {
+  return Math.pow(number, power);
 };
 
-const factorial = function() {
-	
+const factorial = function (number) {
+  if (number === 0) {
+    return 1;
+  } else {
+    return number * factorial(number - 1);
+  }
 };
 
 // Do not edit below this line
@@ -29,5 +49,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
